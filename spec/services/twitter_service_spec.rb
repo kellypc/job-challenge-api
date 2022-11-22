af6f31b6a135a896d1bd9017fd669e82cb6807fd
kellypc <kellycruz.ti@gmail.com>
2022-11-22 14:14:21 -0300

    Remove a unecessary word

--- spec/services/twitter_service_spec.rb
+++ spec/services/twitter_service_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe TwitterService do
   context '#tweet' do
     it 'tweet a message' do
       tweet = TwitterService.send_tweet(tweet_message)
-      debugger
+      
       expect(tweet).to eq("Twitter::Tweet id=1595087860503543808")
     end
   end
